120e33ca0cfb98443ca57116f2ec3b3b0b9856ee
Atsushi Ishida <gipcompany@gmail.com>
2022-08-11 23:05:37 +0900

    refactoring

--- shared-workers/main.js
+++ shared-workers/main.js
@@ -1,17 +1,26 @@
-// 本スクリプトは、メインスレッドで実行されるスクリプトである。
-// メインスレッドは、worker を作成する側のスレッドのため、親スレッドと呼ぶこともある。
-
+// https://developer.mozilla.org/en-US/docs/Web/API/SharedWorker#browser_compatibility
+// All of the browsers may not support SharedWorker.
 if (!!window.SharedWorker) {
-  const worker = new SharedWorker('shared-worker.js', { type: 'module' });
+  const sharedWorker = new SharedWorker('shared-worker.js', { type: 'module' });
+  const inputElement = document.getElementById('inputArea');
+  const outputElement = document.getElementById('outputArea');
+  const encodeElement = document.getElementById('encode');
+  // Both inputElement and outputElement are references
+  // and won't be changed during runtime.
 
-  document.getElementById('encode').onclick = function () {
-    const element = document.getElementById('string');
-    const string = element.value;
-    worker.port.postMessage(string);
+  // https://developer.mozilla.org/en-US/docs/Web/API/SharedWorker/port
+  // port property returns a MessagePort object with which
+  // you main thread can communicate and control the shared worker.
+  const messagePort = sharedWorker.port;
+
+  encodeElement.onclick = () => {
+    const message = inputElement.value;
+    messagePort.postMessage(message);
   };
 
-  worker.port.onmessage = function (event) {
-    const element = document.getElementById('result');
-    element.textContent = event.data;
+  messagePort.onmessage = (messageEvent) => {
+    outputElement.textContent = messageEvent.data;
   };
+
+  messagePort.start();
 }
